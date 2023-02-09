45fcffe0176adf155bd04716e281d39183d80416
gemansipator <gemansipator@gmail.com>
2023-02-09 14:37:26 +0400

    Создайте программу-копирайтера, которая будет считывать данные из одного файла и делать его копию в другом. Однако эта программа так-же будет запоминать откуда и когда она копировала, поэтому условия следующие:
    Программе через консоль указывают абсолютный путь до файла, который необходимо скопировать. Она создаёт его копию у себя в проекте с тем-же названием, но в конце добавляет _копия (Например : был вайл доклад.txt → стал доклад_копия.txt)
    Так-же программа ведёт некий лог файл, где каждый раз пишет свою активность. Файл называется log.txt и каждый раз когда наш копирайте что-то успешно скопировал, то добавляет в журнал новую запись в виде: дата:время был скопирован файл : название_файла → новое_название_файла Пример: 27.01.2023:11:48 был скопирован файл документ.txt → документ_копия.txt
    Обратите внимание, что лог должен дополняться новыми записями, а не при каждом запуске программы очищаться

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,39 @@
 import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
-    public static void main(String[] args) {
-        // определяем объект для каталога
-        File myFile = new File("D://111//myfile1.txt");
-        try
-        {
-            boolean created = myFile.createNewFile();
-            if(created)
-                System.out.println("File has been created");
-        }
-        catch(IOException ex){
+    public static void main(String[] args) throws IOException {
+        LocalDateTime dateTime = LocalDateTime.now();
 
-            System.out.println(ex.getMessage());
-        }
-        System.out.println("File name: " + myFile.getName());
+        //FileWriter класс записи в файл и создания файла как у класса File (тут уже наверно без записи в файл)
+        try (FileWriter writer = new FileWriter("D:\\111\\papka1\\Доклад.txt", true)){ //если false то
+            // каждый запуск программы ПЕРЕЗАПИСЫВАЕТ текст внутри файла,
+            // а ИНАЧЕ, при true, добавляет текст (так работают логи)
+        String text = "Hello World!\n был скопирован файл документ.txt → документ_копия.txt ";
+        writer.write(text); //закидываем (записываем) в поток
+            writer.write(dateTime.format(DateTimeFormatter.ofPattern("dd.MM.uuuu HH:mm:ss" + "\n")));
+        writer.flush();  //непосредствено команда на запись
 
-        try {
-            copyFileUsingStream(new File("D://111//myfile1.txt"), new File("D://111"));
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        try(FileReader reader = new FileReader("D:\\111\\papka1\\Доклад.txt")){
+
+            //reader.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+    copyFileUsingStream(new File("D:\\111\\papka1\\Доклад.txt"), ("Доклад_копия.txt"));
+
+
+
     }
-    private static void copyFileUsingStream(File source, File dest) throws IOException {
+    private static void copyFileUsingStream(File source, String dest) throws IOException {
         InputStream is = null;
         OutputStream os = null;
         try {
@@ -33,9 +44,13 @@ public class Main {
             while ((length = is.read(buffer)) > 0) {
                 os.write(buffer, 0, length);
             }
+
         } finally {
             is.close();
             os.close();
         }
+
     }
+
 }
+
